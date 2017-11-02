b17d724669986e737ddd2ebcf15b6f30473663f3
Aileen Wong <aileen.m.wong@gmail.com>
2017-11-02 11:24:01 -0400

    update model with find by id, create and delete

--- dumplings/models/dumplings.js
+++ dumplings/models/dumplings.js
@@ -2,10 +2,41 @@ const db = require('../db/config');
 const Dumpling = {};
 
 Dumpling.findAll = () => {
-  return db.query
-  (
-    'SELECT * FROM dumplings'
+  return db.query(
+    `
+    SELECT * FROM dumplings
+    `
   );
-}
+};
+
+//this is the function that selects one restaurant
+Dumpling.findById = (id) => {
+  return db.oneOrNone(
+    `
+    SELECT * FROM dumplings
+    WHERE id = $1
+    `, [id]);
+};
+
+//this is the function that selects one restaurant
+Dumpling.create = dumplings => {
+  console.log('inside of create function')
+  // return db.one(
+  //   `
+  //   INSERT INTO dumplings
+  //   (park_name, address, city, state, coordinates, lat, lng, image, website, description, weather, directions, hours)
+  //   VALUES ($/park_name/, $/address/, $/city/, $/state/, $/coordinates/, $/lat/, $/lng/, $/image/, $/website/, $/description/, $/weather/, $/directions/, $/hours/)
+  //   RETURNING *
+  //   `, parks
+  //   );
+};
+
+//this is the function deletes a single item in the database
+Dumpling.delete = (id) => {
+  return db.none(`
+    DELETE FROM dumplings
+    WHERE id = $1
+    `, [id]);
+};
 
 module.exports = Dumpling;
--- dumplings/routes/dumpling-routes.js
+++ dumplings/routes/dumpling-routes.js
@@ -5,16 +5,6 @@ const dumplingController = require('../controllers/dumpling-controller');
 
 dumplingRoutes.get('/', dumplingController.index);
 
-// // still have to import the quote data
-// const quoteInfo = require('../db/quotes-data');
-
-// // the root route, `/quotes`
-// quoteRoutes.get('/', (req, res) => {
-//   res.json({
-//     message: 'ok',
-//     data: quotes,
-//   });
-// });
 
 // need to export the files
 module.exports = dumplingRoutes;
