5762b7967b5b69a6837f5d25caf890f6bda53d14
Aileen Wong <aileen.m.wong@gmail.com>
2017-11-07 13:56:40 -0500

    add delete and show 1 to controller

--- dumplings/controllers/dumpling-controller.js
+++ dumplings/controllers/dumpling-controller.js
@@ -16,4 +16,29 @@ dumplingController.index = (req, res) => {
     });
 }
 
+//renders the data from a single item in the database
+dumplingController.show = (req, res) => {
+  Dumpling.findById(req.params.id)
+  .then(dumpling => {
+    res.render('./#', {
+      data: dumpling,
+    })
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+}
+
+//deletes an item from the database
+dumplingController.delete = (req, res) => {
+  Dumpling.delete(req.params.id)
+  .then(() => {
+    res.redirect('/#');
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+};
+
 module.exports = dumplingController;
